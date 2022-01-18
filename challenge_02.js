f7e1fbe4fa2dc549ec126f14ee52d15209215604
Peter <schmidt.multimedia@gmail.com>
2022-01-18 13:05:43 +0100

    Lösungsbeispiel

--- challenge_02.js
+++ challenge_02.js
@@ -12,8 +12,27 @@ Alter 6-12 // Saft
 Alter 13-17 // Cola
 18 + // Wein
 
---> "Max trinkt Cola."
+-->zB: "Max trinkt Cola."
 
 /* Getränke-Challenge */
 
- 
+const firstName = prompt("Bitte Namen eingeben:");
+const age =  parseInt(prompt("Bitte Alter eingeben:")); // "2" --> 2
+
+switch (true) {
+    case (age >= 0) && (age <= 5) :
+        console.log(firstName + " trinkt Milch");
+        break;
+    case (age >= 6) && (age <= 12):
+        console.log(firstName + " trinkt Saft");
+        break;
+    case (age >= 13) && (age <= 17):
+        console.log(firstName + " trinkt Cola");
+        break;
+    case (age >= 18) && (age <= 130):
+        console.log(firstName + " trinkt Wein");
+        break;   
+    default:
+        console.log("Bitte Tee trinken!");   
+        break;
+}
